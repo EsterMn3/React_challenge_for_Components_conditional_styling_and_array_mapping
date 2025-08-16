1ec9df88e44b6f4b63dc25944a46418e009674f8
Ester Minja <145455804+EsterMn3@users.noreply.github.com>
2025-08-17 00:53:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="./challenge react.webp" className="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Jules Moran</h1>
+      <p>
+        Full-stack web developer and teacher at Udemy. When not coding or
+        preparing a course, I like to play board games, to cook (and eat), or to
+        just enjoy the Portuguese sun at the beach.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skillObject={skill} key={skill.name} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skillObject }) {
+  return (
+    <p className="skill" style={{ backgroundColor: skillObject.color }}>
+      {skillObject.skill}
+      {skillObject.level == "advanced"
+        ? "💪"
+        : skillObject.level == "beginner"
+        ? "👶"
+        : "👍"}
+    </p>
+  );
+}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
